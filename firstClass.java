0da3762ddcd3e83cbe9d69912a7d160eee84c1c4
rahulchouhana100 <rahulchouhan8991@gmail.com>
2023-01-12 21:11:20 +0530

    added changes file

--- firstClass.java
+++ firstClass.java
@@ -1,14 +1,15 @@
 import java.util.*;
 
 public class firstClass {
-    public static void calculateSum(){
-        Scanner Sc = new Scanner(System.in);
-        int a = Sc.nextInt();
-        int b = Sc.nextInt();
+    public static void calculateSum(int a , int b){
+        
         int sum = a + b;
         System.out.print("output is :"  + sum ) ;
     }
     public static void main( String args[] ){
-       calculateSum();
+       Scanner Sc = new Scanner(System.in);
+        int a = Sc.nextInt();
+        int b = Sc.nextInt();
+       calculateSum(a,b);
     }
 }
