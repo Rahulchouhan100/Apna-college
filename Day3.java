69b9bb65c8e87bffbf6d9f99e066600e13d3871a
Rahulchouhan100 <rahulchouhan8991@gmail.com>
2023-01-16 21:46:42 +0530

    odd&even

---
+++ Day3.java
@@ -0,0 +1,27 @@
+// conditional statement question
+
+// Q1. Print if a number is odd or even 
+/* 
+
+import java.util.*;
+
+public class firstClass {
+
+    public static void main( String args[] ){
+       Scanner Sc = new Scanner(System.in);
+       int number = Sc.nextInt();
+       if(number%2 == 0){
+         System.out.print("even");
+       }else{
+        System.out.print("odd");
+       }
+    }
+}
+
+
+*/
+
+
+
+
+
