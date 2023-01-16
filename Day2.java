d8ad88b9ec43fd169049151e5e3a7e99fe8ba06e
Rahulchouhan100 <rahulchouhan8991@gmail.com>
2023-01-16 21:37:33 +0530

    day2

---
+++ Day2.java
@@ -0,0 +1,16 @@
+ // q1. char a = 97;
+//        char b = 98;
+//        char c = 99;
+
+import java.util.*;
+public class firstClass {
+
+    public static void main( String args[] ){
+       char a = 'a';
+       char b = 'b';
+
+        System.out.print(b-a);
+    }
+}
+
+
